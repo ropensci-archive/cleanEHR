c03d43a92e6b6801543a31ee74cc6ace6a3a8047
Sinan Shi <s.shi@ucl.ac.uk>
2016-02-23 22:00:06 +0000

    error handling; return NULL instead of error when code.no.prefix cannot be found.

--- R/getPatientItems.r
+++ R/getPatientItems.r
@@ -8,14 +8,23 @@ getPatient1dItem <- function (data1d, item.code, patient.id) {
 }
 
 
-#' retrieve one column of 2d data from one or multiple patients
+#' retrieve one column of 2d data from one item of one patients
 #' @param data2d
 #' @param item
 #' @param patient.id
 getPatient2dItem <- function (data2d, item.code, patient.id) {
     code.no.prefix <- removeIdPrefix(item.code)
-    # TODO: convert to POSIX while reading the xml file. 
-    return(data.frame(time=xmlTime2POSIX(rownames(data2d[[patient.id]])),
-                      val=as.character(data2d[[patient.id]][, code.no.prefix]),
-                      stringsAsFactors=FALSE))
+    # TODO: convert to POSIX while reading the xml file.
+    tryCatch(
+             time <- xmlTime2POSIX(rownames(data2d[[patient.id]])),
+             error=function(e){
+                 cat("patient.id =", patient.id, "cannot be found!\n")
+                 print(e)
+             })
+    if(code.no.prefix %in% colnames(data2d[[patient.id]])) {
+        val <- as.character(data2d[[patient.id]][, code.no.prefix])
+        return(data.frame(time, val, stringsAsFactors=FALSE))
+    }
+    else
+        return(NULL)
 }
