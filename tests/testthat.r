0e65f934193835a6d51be0bdbed3d80cf6fc5796
Sinan Shi <s.shi@ucl.ac.uk>
2016-03-10 11:03:51 +0000

    adding testthat testing framework

---
+++ tests/testthat.r
@@ -0,0 +1,4 @@
+library(testthat)
+library(dataplay)
+
+test_check("dataplay")
---
+++ tests/testthat/test_xml_data.r
@@ -0,0 +1,9 @@
+context("Tests of the xml parser")
+
+
+test_that("load xml file", 
+{
+
+    r <- xmlLoad("../../../CriticalCare/anon_CC.xml")
+    ccd <-xml2Data(r, seq(5))
+})
