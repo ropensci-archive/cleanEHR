a3156bef17e30da00bb955c52e60131b6bcd4db6
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-04 16:24:03 +0100

    do not load the file when testing.

--- tests/testthat.r
+++ tests/testthat.r
@@ -1,6 +1,6 @@
 library(testthat)
 library(ccdata)
 
-if (!exists(test_xml))
-    test_xml <- xmlLoad("../CriticalCare/anon_CC.xml")
-test_check("dataplay")
+#if (!exists(test_xml))
+#    test_xml <- xmlLoad("../CriticalCare/anon_CC.xml")
+test_check("ccdata")
