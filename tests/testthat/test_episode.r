60912d844b16b192823b087e65b7ab83bdf39a1f
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-10 17:11:07 +0100

    fixing the bug of various id inputs of episodes.

--- tests/testthat/test_episode.r
+++ tests/testthat/test_episode.r
@@ -16,3 +16,25 @@ test_that("add 1D item.", {
     expect_error(ep + data.frame(id=c("xx", "xx"),
                                  val=c(1,2))) # duplication of 1d data
 })
+
+test_that("add an empty episode.", {
+    new <- ccd + ccEpisode()
+    expect_equal(new@npatient, ccd@npatient + 1)
+})
+
+test_that("check cases of duplicated ids", {
+    # test PAS number
+    p <- ccPatient()
+    p <- p + ccEpisode(pas_number="pas_num_1")
+    # newly added episode pas_number is 'NULL', and the pas_number should
+    # maintain the original value.
+    expect_match('pas_num_1', (p + ccEpisode())@pas_number)
+    # pas_number should always be the same for one single patient.
+    expect_error(p + ccEpisode(pas_number='pas_number_2'))
+
+    # test NHS number
+    p2 <- ccPatient()
+    p2 <- p2 + ccEpisode(nhs_number="nhs_num_1")
+    expect_match('nhs_num_1', (p2 + ccEpisode())@nhs_number)
+    expect_error(p2 + ccEpisode(nhs_number='nhs_number_2'))
+})
