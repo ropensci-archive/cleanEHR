b2b286cde3fe69ea0bd409c8680b95b450a3be04
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-11 13:40:58 +0100

    ready to ship to DHS - aggPaitents not ready.

--- R/aggPatients.r
+++ R/aggPatients.r
@@ -1,14 +1,29 @@
-setGeneric("aggPatients",
-           function(record) {
-               standardGeneric("aggPatients")
-           })
-
-#' Aggregate episodes with the same patient ids (e.g. nhs_number or pas_number)
-#' @param record ccRecord
-setMethod("aggPatients", 
-          c("ccRecord"),
-          function(record) {
+##' return selected index from nhs_number or pas_number
+##' @param record ccRecord
+##' @param id character, either the nhs_number or pas_number
+##' @return index vector 
 
 
-          })
- 
+#=============
+
+
+patientIndex <- function (record, id) {
+    index_nhs <- record['NULL']
+
+#    index_pas <- record@pas_numbers[id]$index
+    print(index_nhs)
+    print(class(index_nhs))
+ #   print(index_pas)
+
+#    if (is.numeric(index_nhs) & is.numeric(index_pas))
+#        stop('found both maching nhs number and pas number!')
+#
+#    if (all(is.na(c(index_nhs, index_pas))))
+#        return(NA)
+#    else {
+#        if (is.na(index_nhs))
+#            return(index_pas)
+#        else
+#            return(index_nhs)
+#    }
+}
