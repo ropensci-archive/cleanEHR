4497b022aa44c929a6c0612c2f30a1eef0546183
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-16 06:20:15 +0100

    load data.checklist in .onLoad()

--- R/zzz.r
+++ R/zzz.r
@@ -1,5 +1,6 @@
 .onLoad <- function(libname = find.package("ccdata"), pkgname = "ccdata") {
     ccdata.env <<- new.env()
+    data("data.checklist", package="ccdata")
     assign('code_pas_number',  getItemInfo("PAS number")["NHIC_code"], envir=ccdata.env)
     assign('code_nhs_number',  getItemInfo("NHS number")["NHIC_code"], envir=ccdata.env)
     assign('code_episode_id', 
