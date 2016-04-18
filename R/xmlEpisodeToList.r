91f75a52490079f84519c8aca346e124cb5622b2
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-18 15:46:39 +0100

    using loop to update values

--- R/xmlEpisodeToList.r
+++ R/xmlEpisodeToList.r
@@ -14,61 +14,93 @@ xmlEpisodeToList <- function(episode_node) {
                 warning("XML structure error:", label)
             }
             else {
-                if (.which.type(label) == "item1d") {
+                # here we check if the id is 1d instead of checking the
+                # parent node size, because the parent node size can be 1 due
+                # to missing data of 2d items, which should not be recorded as
+                # valide data, as a result missing data will be ignored. 
+                if (.which.type(label) == "item1d") { 
                     node_env$data[[label]][[label]] <- xmlValue(node)
                 }
 
                 else {
+#                    node_ <- xmlParent(node)
+#                    parent_size <- xmlSize(xmlParent(node_))
+#
+#                    for (i in seq(parent_size)) {
+#                        if (xmlName(node_) == xmlName(xmlParent(node_)[[1]]))
+#                        {
+#                            node_name <- xmlName(node_)
+#                            if (i > 1) 
+#                                node_ <- getSibling(node_)
+#
+#
+#                            if (is.null(node_env$data[[label]][[node_name]]))
+#                                node_env$data[[label]][[node_name]] <- vector()
+#
+#                            node_env$data[[label]][[node_name]] <-
+#                                c(node_env$data[[label]][[node_name]],
+#                                  xmlValue(node_[[1]]))
+#                        }
+#                    }
+#                }
+#            }
+#        }
+#
+
+
+
+
+
 
                     parent_size <- xmlSize(xmlParent(xmlParent(node)))
-                    sib <- getSibling(xmlParent(node))
-
-                    if (parent_size == 2) {
-                        if (!is.null(sib)) {
-                            old_data <- node_env$data[[label]][[label]]
-
-                            if(is.null(old_data))
-                                node_env$data[[label]][[label]] <- vector()
-                            node_env$data[[label]][[label]] <- c(old_data, xmlValue(node))
-
-                            old_sibling <- node_env$data[[label]][[xmlName(sib)]]
-                            if (is.null(old_sibling))
-                                node_env$data[[label]][[xmlName(sib)]] <- vector()
-                            node_env$data[[label]][[xmlName(sib)]] <-
-                                c(old_sibling, xmlValue(sib))
-                        }
-                    }
-                    else if (parent_size == 3) {
-                        if (!is.null(sib)){
-                            sibsib <- getSibling(sib)
-                            if (!is.null(sibsib)) {
-                                sibling1_name <- xmlName(sib)
-                                sibling2_name <- xmlName(sibsib)
-
-                                old_data <- node_env$data[[label]][[label]]
-                                old_sibling1 <- node_env$data[[label]][[sibling1_name]]
-                                old_sibling2 <- node_env$data[[label]][[sibling2_name]]
-
-                                if (is.null(old_data))
-                                    node_env$data[[label]][[label]] <- vector()
-                                if (is.null(old_sibling1))
-                                    node_env$data[[label]][[sibling1_name]] <- vector()
-                                if (is.null(old_sibling2))
-                                    node_env$data[[label]][[sibling2_name]] <- vector()
-
-                                node_env$data[[label]][[label]] <- xmlValue(node)
-                                node_env$data[[label]][[sibling1_name]] <-
-                                    xmlValue(sib)
-                                node_env$data[[label]][[sibling2_name]] <-
-                                    xmlValue(sibsib)
-
+                            sib <- getSibling(xmlParent(node))
+        
+                            if (parent_size == 2) {
+                                if (!is.null(sib)) {
+                                    old_data <- node_env$data[[label]][[label]]
+        
+                                    if(is.null(old_data))
+                                        node_env$data[[label]][[label]] <- vector()
+                                    node_env$data[[label]][[label]] <- c(old_data, xmlValue(node))
+        
+                                    old_sibling <- node_env$data[[label]][[xmlName(sib)]]
+                                    if (is.null(old_sibling))
+                                        node_env$data[[label]][[xmlName(sib)]] <- vector()
+                                    node_env$data[[label]][[xmlName(sib)]] <-
+                                        c(old_sibling, xmlValue(sib))
+                                }
                             }
+                            else if (parent_size == 3) {
+                                if (!is.null(sib)){
+                                    sibsib <- getSibling(sib)
+                                    if (!is.null(sibsib)) {
+                                        sibling1_name <- xmlName(sib)
+                                        sibling2_name <- xmlName(sibsib)
+        
+                                        old_data <- node_env$data[[label]][[label]]
+                                        old_sibling1 <- node_env$data[[label]][[sibling1_name]]
+                                        old_sibling2 <- node_env$data[[label]][[sibling2_name]]
+        
+                                        if (is.null(old_data))
+                                            node_env$data[[label]][[label]] <- vector()
+                                        if (is.null(old_sibling1))
+                                            node_env$data[[label]][[sibling1_name]] <- vector()
+                                        if (is.null(old_sibling2))
+                                            node_env$data[[label]][[sibling2_name]] <- vector()
+        
+                                        node_env$data[[label]][[label]] <- xmlValue(node)
+                                        node_env$data[[label]][[sibling1_name]] <-
+                                            xmlValue(sib)
+                                        node_env$data[[label]][[sibling2_name]] <-
+                                            xmlValue(sibsib)
+        
+                                    }
+                                }
+                            }
+        
                         }
                     }
-
                 }
-            }
-        }
 
         #Go one level denode_env$dataer
         for (i in 1 : num.children) {
@@ -84,7 +116,7 @@ xmlEpisodeToList <- function(episode_node) {
     node_env$data <- list() # store data from the XML traverser 
     traverseNode(episode_node)
     node_env$ccdata <- list() # format can be directely called by ccEpisode.
-    
+
     # rearrange the vector data from node_env$data to ccdata format.
     # Regarding to the performance, XML traverser only put all the data in a
     # vector form, as vector appending is much faster than data.frame
@@ -95,7 +127,8 @@ xmlEpisodeToList <- function(episode_node) {
                    if (.which.type(names(x)) == "item1d" & is.character(x[[1]]))
                        node_env$ccdata[[names(x)]] <- as.character(x)
                    else
-                       stop("wrong simple data", names(x))
+                       print(node_env$ccdata[[names(x)]])
+                       stop("wrong simple data ", names(x))
                }
                else if (len == 2) { # 2d item (items in time)
                    nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]))
@@ -107,11 +140,14 @@ xmlEpisodeToList <- function(episode_node) {
                    nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]),
                            .which.type(names(x)[3]))
                    label <- names(x)[nm == "item2d"]
+                   print(paste("=====", label))
+                   print(names(x))
+                   print(nm)
+                   print(node_env$ccdata[[label]])
+                   #print(data.frame(x[[1]], x[[2]], x[[3]]))
                    node_env$ccdata[[label]] <- data.frame(x[[1]], x[[2]], x[[3]])
                    names(node_env$ccdata[[label]]) <- nm
-               }
-})
-
+                                  }})
 
     return(node_env$ccdata)
 }
