af2e397f9e7cf9f68af8d94cb8161d887a49d303
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-22 10:31:31 +0100

    further tiding up

--- R/repeatBinIndex.r
+++
@@ -1,7 +0,0 @@
-#' Provide an index of consequtively repeated items from a a given a binary vector.
-#' @param v vector only contains integer 0 or 1
-#' @return a vector without repeating.
-repeatBinIndex <- function(v) {
-    v_ <- c(0, v)[-(length(v)+1)]
-    return(v * v_ == 1)
-}
