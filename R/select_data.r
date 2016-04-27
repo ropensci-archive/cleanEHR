e3a3f9dbfee1541a0f07a688ec789e6ecf44a57e
Sinan Shi <s.shi@ucl.ac.uk>
2016-04-27 15:08:11 +0100

    split 1d and 2d data result

--- R/select_data.r
+++ R/select_data.r
@@ -1,4 +1,6 @@
-#' Retrieve one item at a time from the ccRecord
+#' Retrieve one item at a time from the ccRecord, NOTE: items with meta data,
+#' i.e. three columns case is not valide for this function, should be improved
+#' in the future.
 #' @param record ccRecord
 #' @param item_id NHIC code
 #' @export readOneItem
@@ -9,12 +11,14 @@ readOneItem <- function(record, item_id, convert_type=NULL, as.POSIX=FALSE,
     if (is.null(short_name))
         short_name <- "NULL"
     env <- new.env()
-    env$tb <- list()
+    # to store 1d data, in the 2d data case, this list will keep empty
+    env$tb_1 <- list() 
+    env$tb_2 <- list() 
+
     lapply(record@patients, 
            function(x) {
                lapply(x@episodes, 
                       function(x) {
-                          pos <- length(env$tb) + 1
                           if (as.POSIX != FALSE) {
                               x@data[[item_id]]$time <-
                                   xmlTime2POSIX(x@data[[item_id]]$time)
@@ -31,7 +35,8 @@ readOneItem <- function(record, item_id, convert_type=NULL, as.POSIX=FALSE,
                           }
 
                           if (length(x@data[[item_id]]) == 1) {
-                              env$tb[[pos]] <- data.frame(nhs_number=x@nhs_number, 
+                              pos <- length(env$tb_1) + 1
+                              env$tb_1[[pos]] <- data.table(nhs_number=x@nhs_number, 
                                                           pas_number=x@pas_number,
                                                           episode_id=x@episode_id, 
                                                           site_id=x@site_id,
@@ -41,8 +46,9 @@ readOneItem <- function(record, item_id, convert_type=NULL, as.POSIX=FALSE,
                                                           val=x@data[[item_id]])
                           }
                           else if (length(x@data[[item_id]]) == 2) {
+                              pos <- length(env$tb_2) + 1
                               nr <- nrow(x@data[[item_id]])
-                              env$tb[[pos]] <-
+                              env$tb_2[[pos]] <-
                                   data.table(nhs_number = rep(x@nhs_number, nr), 
                                              pas_number = rep(x@pas_number, nr),  
                                              episode_id = rep(x@episode_id, nr),
@@ -54,18 +60,25 @@ readOneItem <- function(record, item_id, convert_type=NULL, as.POSIX=FALSE,
                           }
                       })
            })
-    if (unlist == TRUE) {
-        return(rbindlist(env$tb))
-    }
-    return(env$tb)
+    
+    if (unlist == TRUE) 
+        return(list(data1d=rbindlist(env$tb_1), data2d=rbindlist(env$tb_2)))
+    else
+        return(list(data1d=env$tb_1, data2d=env$tb_2))
 }
 
 #' @export readItems
-readItems <- function(record, item_ids, ...) {
+readItems <- function(record, item_ids, split=TRUE, ...) {
     if (any(duplicated(item_ids)))
         stop("item ids should not be duplicated.")
-    tt <- list()
-    for (id in item_ids) 
-        tt[[id]] <- readOneItem(record, id, unlist=TRUE)
-    return(rbindlist(tt))
+    tt1d <- list()
+    tt2d <- list()
+    for (id in item_ids) {
+        tt <- readOneItem(record, id, unlist=TRUE)
+        if (nrow(tt$data1d) != 0 )
+            tt1d[[id]] <- tt$data1d
+        else 
+            tt2d[[id]] <- tt$data2d
+    }
+    return(list(data1d=rbindlist(tt1d), data2d=rbindlist(tt2d)))
 }
--- R/xmlEpisodeToList.r
+++ R/xmlEpisodeToList.r
@@ -13,131 +13,133 @@ xmlEpisodeToList <- function(episode_node) {
             if (is.null(.which.type(label))) {
                 warning("XML structure error:", label)
             }
-            else if (.which.type(label) == "item1d") {
-                node_env$data[[label]][[label]] <- xmlValue(node)
-            }
-
             else {
-                parent_size <- xmlSize(xmlParent(xmlParent(node)))
-                sib <- getSibling(xmlParent(node))
-
-                if (parent_size == 2) {
-                    if (!is.null(sib)) {
-                        old_data <- node_env$data[[label]][[label]]
-
-                        if(is.null(old_data))
-                            node_env$data[[label]][[label]] <- vector()
-                        node_env$data[[label]][[label]] <- c(old_data, xmlValue(node))
-
-                        old_sibling <- node_env$data[[label]][[xmlName(sib)]]
-                        if (is.null(old_sibling))
-                            node_env$data[[label]][[xmlName(sib)]] <- vector()
-                        node_env$data[[label]][[xmlName(sib)]] <-
-                            c(old_sibling, xmlValue(sib))
-                    }
+                if (.which.type(label) == "item1d") {
+                    node_env$data[[label]][[label]] <- xmlValue(node)
                 }
-                else if (parent_size == 3) {
-                    if (!is.null(sib)){
-                        sibsib <- getSibling(sib)
-                        if (!is.null(sibsib)) {
-                            sibling1_name <- xmlName(sib)
-                            sibling2_name <- xmlName(sibsib)
 
+                else {
+
+                    parent_size <- xmlSize(xmlParent(xmlParent(node)))
+                    sib <- getSibling(xmlParent(node))
+
+                    if (parent_size == 2) {
+                        if (!is.null(sib)) {
                             old_data <- node_env$data[[label]][[label]]
-                            old_sibling1 <- node_env$data[[label]][[sibling1_name]]
-                            old_sibling2 <- node_env$data[[label]][[sibling2_name]]
 
-                            if (is.null(old_data))
+                            if(is.null(old_data))
                                 node_env$data[[label]][[label]] <- vector()
-                            if (is.null(old_sibling1))
-                                node_env$data[[label]][[sibling1_name]] <- vector()
-                            if (is.null(old_sibling2))
-                                node_env$data[[label]][[sibling2_name]] <- vector()
+                            node_env$data[[label]][[label]] <- c(old_data, xmlValue(node))
 
-                            node_env$data[[label]][[label]] <- xmlValue(node)
-                            node_env$data[[label]][[sibling1_name]] <-
-                                xmlValue(sib)
-                            node_env$data[[label]][[sibling2_name]] <-
-                                xmlValue(sibsib)
-
+                            old_sibling <- node_env$data[[label]][[xmlName(sib)]]
+                            if (is.null(old_sibling))
+                                node_env$data[[label]][[xmlName(sib)]] <- vector()
+                            node_env$data[[label]][[xmlName(sib)]] <-
+                                c(old_sibling, xmlValue(sib))
                         }
                     }
-                }
+                    else if (parent_size == 3) {
+                        if (!is.null(sib)){
+                            sibsib <- getSibling(sib)
+                            if (!is.null(sibsib)) {
+                                sibling1_name <- xmlName(sib)
+                                sibling2_name <- xmlName(sibsib)
 
+                                old_data <- node_env$data[[label]][[label]]
+                                old_sibling1 <- node_env$data[[label]][[sibling1_name]]
+                                old_sibling2 <- node_env$data[[label]][[sibling2_name]]
+
+                                if (is.null(old_data))
+                                    node_env$data[[label]][[label]] <- vector()
+                                if (is.null(old_sibling1))
+                                    node_env$data[[label]][[sibling1_name]] <- vector()
+                                if (is.null(old_sibling2))
+                                    node_env$data[[label]][[sibling2_name]] <- vector()
+
+                                node_env$data[[label]][[label]] <- xmlValue(node)
+                                node_env$data[[label]][[sibling1_name]] <-
+                                    xmlValue(sib)
+                                node_env$data[[label]][[sibling2_name]] <-
+                                    xmlValue(sibsib)
+
+                            }
+                        }
+                    }
+
+                }
             }
         }
-    }
 
-    #Go one level denode_env$dataer
-    for (i in 1 : num.children) {
-        if (xmlValue(node) == "") {
-            warning("XML structure is wrong ", xmlName(node))
-            next
+        #Go one level denode_env$dataer
+        for (i in 1 : num.children) {
+            if (xmlValue(node) == "") {
+                warning("XML structure is wrong ", xmlName(node))
+                next
+            }
+            traverseNode(node[[i]]) #the i-th child of node
         }
-        traverseNode(node[[i]]) #the i-th child of node
     }
-}
 
-node_env <- new.env()
-node_env$data <- list() # store data from the XML traverser 
-traverseNode(episode_node)
-node_env$ccdata <- list() # format can be directely called by ccEpisode.
-
-# rearrange the vector data from node_env$data to ccdata format.
-# Regarding to the performance, XML traverser only put all the data in a
-# vector form, as vector appending is much faster than data.frame
-# appending.
-lapply(node_env$data, function(x) {
-           len <- length(x)
-           if (len == 1) { # 1d item (simple item)
-               if (.which.type(names(x)) == "item1d" & is.character(x[[1]]))
-                   node_env$ccdata[[names(x)]] <- as.character(x)
-               else
-                   stop("wrong simple data", names(x))
-           }
-           else if (len == 2) { # 2d item (items in time)
-               nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]))
-               label <- names(x)[nm == "item2d"]
-               if (length(label) == 1) {
-                   node_env$ccdata[[label]] <- data.frame(x[[1]], x[[2]])
-                   names(node_env$ccdata[[label]]) <- nm
+    node_env <- new.env()
+    node_env$data <- list() # store data from the XML traverser 
+    traverseNode(episode_node)
+    node_env$ccdata <- list() # format can be directely called by ccEpisode.
+    
+    # rearrange the vector data from node_env$data to ccdata format.
+    # Regarding to the performance, XML traverser only put all the data in a
+    # vector form, as vector appending is much faster than data.frame
+    # appending.
+    lapply(node_env$data, function(x) {
+               len <- length(x)
+               if (len == 1) { # 1d item (simple item)
+                   if (.which.type(names(x)) == "item1d" & is.character(x[[1]]))
+                       node_env$ccdata[[names(x)]] <- as.character(x)
+                   else
+                       stop("wrong simple data", names(x))
                }
-           }
-           else if (len == 3) { # time data with meta data, i.e. 3 columns
-               nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]),
-                       .which.type(names(x)[3]))
-               label <- names(x)[nm == "item2d"]
-
-               # usually label, i.e. item2d should be unique, however just
-               # in case of incomplete 4-column data in which more than 1
-               # item2d will be found.
-               for (i in label) {
-                   node_env$ccdata[[i]] <- data.frame(x[[1]], x[[2]], x[[3]])
-                   names(node_env$ccdata[[i]]) <- nm
+               else if (len == 2) { # 2d item (items in time)
+                   nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]))
+                   label <- names(x)[nm == "item2d"]
+                   if (length(label) == 1) {
+                       node_env$ccdata[[label]] <- data.frame(x[[1]], x[[2]])
+                       names(node_env$ccdata[[label]]) <- nm
+                   }
                }
-           }
-           # 4 columns case only happens in laboratory microbiology
-           # culture, where item labels 0186 (Site), 0187 (Oranism) 
-           # and 0189 (Sensitivity) share the same time label and being
-           # allocated in the same XML block. In `ccdata` a 4 columns data
-           # frame will be created and duplicated under the item names. 
-           else if (len == 4) {
-               nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]), 
-                       .which.type(names(x)[3]), .which.type(names(x)[4]))
-               label <- names(x)[nm == "item2d"]
-               for(i in label) {
-                   node_env$ccdata[[i]] <- data.frame(x[[1]], x[[2]],
-                                                      x[[3]], x[[4]])
-                   names(node_env$ccdata[[i]]) <- nm
-               }
-           }
-           else{
-               print(x)
-               stop("0 or more than 4 columns here!")
+               else if (len == 3) { # time data with meta data, i.e. 3 columns
+                   nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]),
+                           .which.type(names(x)[3]))
+                   label <- names(x)[nm == "item2d"]
 
-           }
+                   # usually label, i.e. item2d should be unique, however just
+                   # in case of incomplete 4-column data in which more than 1
+                   # item2d will be found.
+                   for (i in label) {
+                       node_env$ccdata[[i]] <- data.frame(x[[1]], x[[2]], x[[3]])
+                       names(node_env$ccdata[[i]]) <- nm
+                   }
+               }
+               # 4 columns case only happens in laboratory microbiology
+               # culture, where item labels 0186 (Site), 0187 (Oranism) 
+               # and 0189 (Sensitivity) share the same time label and being
+               # allocated in the same XML block. In `ccdata` a 4 columns data
+               # frame will be created and duplicated under the item names. 
+               else if (len == 4) {
+                   nm <- c(.which.type(names(x)[1]), .which.type(names(x)[2]), 
+                           .which.type(names(x)[3]), .which.type(names(x)[4]))
+                   label <- names(x)[nm == "item2d"]
+                   for(i in label) {
+                       node_env$ccdata[[i]] <- data.frame(x[[1]], x[[2]],
+                                                          x[[3]], x[[4]])
+                       names(node_env$ccdata[[i]]) <- nm
+                   }
+               }
+               else{
+                   print(x)
+                   stop("0 or more than 4 columns here!")
+               
+               }
 })
 
 
-return(node_env$ccdata)
+    return(node_env$ccdata)
 }
