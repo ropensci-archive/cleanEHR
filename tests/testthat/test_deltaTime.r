553bb28963d8c5a2433cb78c66ce8a3e4ca832ed
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-03 22:31:39 +0100

    adding tests for deltaTime

---
+++ tests/testthat/test_deltaTime.r
@@ -0,0 +1,29 @@
+context("Testing deltaTime")
+
+test_that("convert to delta time and convert it back",
+{
+    dt <- deltaTime(ccd, anonymised=TRUE)
+    time_restore <- for_each_episode(dt, 
+                     function(episode) {
+                         env <- environment() 
+                         lapply(episode@data,
+                                function(x) {
+                                    if (length(x)>1) {
+                                        env$episode@admin_icu_time + x$time
+                                    }
+                                })
+                     })
+
+    time_origin <- for_each_episode(ccd, 
+                     function(episode) {
+                         lapply(episode@data,
+                                function(x) {
+                                    if (length(x)>1) {
+                                        xmlTime2POSIX(x$time)
+                                    }
+                                })
+                     })
+
+    expect_equal(time_origin, time_restore)
+
+})
