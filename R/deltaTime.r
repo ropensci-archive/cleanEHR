00a4875a97cf5d1e715647fe64fe4c2b3b90b2e3
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-03 22:00:33 +0100

    allow POSIX format written in admin_icu_time slot of ccEpisode

--- R/deltaTime.r
+++ R/deltaTime.r
@@ -26,32 +26,26 @@ find.episode.time <- function(episode) {
 #' convert calendar time data in a record to delta time comparing to the ICU
 #' admission time. 
 #' @export deltaTime
-deltaTime <- function(record, anonymised=TRUE) {
-    
+deltaTime <- function(record, anonymised=FALSE) {
+
     # for anonymised data only: 
     # convert hash admin time to the earliest time of the record
     if (anonymised == TRUE) {
         admin_disc_time <- for_each_episode(record, find.episode.time)
         # NOTE: please try Map here which may look neater. 
-        # +0.001 minute to avoid as.character avoid time information when the 
-        # time is exactly 0h.
         for (p in seq(record@patients)) {
             for(e in seq(record@patients[[p]]@episodes)) {
-                record@patients[[p]]@episodes[[e]]@admin_icu_time <- 
-                    gsub(" ", "T",
-                         as.character(admin_disc_time[[p]][[e]][1]+0.01))
-                record@patients[[p]]@episodes[[e]]@discharge_icu_time <- 
-                    gsub(" ", "T",
-                         as.character(admin_disc_time[[p]][[e]][2]+0.01))
+                record@patients[[p]]@episodes[[e]]@admin_icu_time <- admin_disc_time[[p]][[e]][1]
+                record@patients[[p]]@episodes[[e]]@discharge_icu_time <- admin_disc_time[[p]][[e]][2]
             }
         }
     }
 
-
     update_time <- function(ep) {
         env <- environment()
         admin_icu_time <- ep@admin_icu_time
-        lapply(ep@data, 
+        cat("-")
+        lapply(ep@data,
                function(data) {
                    if (length(data) > 1) {
                        data$time <- xmlTime2POSIX(data$time) - 
@@ -62,18 +56,17 @@ deltaTime <- function(record, anonymised=TRUE) {
     }
 
     record <- ccRecord() + for_each_episode(record, update_time)
-    
-    # NOTE: duplicated!!! 
-    for (p in seq(record@patients)) {
-        for(e in seq(record@patients[[p]]@episodes)) {
-            record@patients[[p]]@episodes[[e]]@admin_icu_time <- 
-                gsub(" ", "T",
-                     as.character(admin_disc_time[[p]][[e]][1]+0.01))
-            record@patients[[p]]@episodes[[e]]@discharge_icu_time <- 
-                gsub(" ", "T",
-                     as.character(admin_disc_time[[p]][[e]][2]+0.01))
+
+    if (anonymised == TRUE) {
+        for (p in seq(record@patients)) {
+            for(e in seq(record@patients[[p]]@episodes)) {
+                record@patients[[p]]@episodes[[e]]@admin_icu_time <- 
+                    admin_disc_time[[p]][[e]][1]
+                record@patients[[p]]@episodes[[e]]@discharge_icu_time <- 
+                    admin_disc_time[[p]][[e]][2]
+            }
         }
     }
-
+    
     return(record)
 }
