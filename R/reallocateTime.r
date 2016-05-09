f8f0fb9cf96df2007cb35d2bfe00c84a7a4ac5ab
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-09 10:36:04 +0100

    reallocate time variable in records

--- R/reallocateTime.r
+++ R/reallocateTime.r
@@ -8,3 +8,23 @@ reallocateTime <- function(d, t_discharge, frequency) {
     stopifnot(class(d$time) == "numeric")
     return(reallocateTime_(d_, t_discharge, frequency))
 }
+
+
+#' @export reallocateTimeRecord
+reallocateTimeRecord <- function(record, delta=0.5) {
+    newdata <- for_each_episode(record, 
+                                function(e) {
+                                    cat('-')
+                                    env <- environment()
+                                    lapply(e@data, 
+                                           function(d) {
+                                               if (length(d) > 1) {
+                                                   maxtime <- env$e@discharge_icu_time - env$e@admin_icu_time
+                                                   maxtime <- as.numeric(maxtime, units="hours")
+                                                   return(reallocateTime(d, maxtime, delta))
+                                               } else 
+                                                   return(d)
+                                           })
+                                })
+    return(ccRecord() + newdata)
+}
