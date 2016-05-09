ae5045db15fa6bbb068931250b57c44bd69e7016
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-09 16:50:52 +0100

    check admin/discharge time format and availability

--- R/reallocateTime.r
+++ R/reallocateTime.r
@@ -10,21 +10,61 @@ reallocateTime <- function(d, t_discharge, frequency) {
 }
 
 
+findMaxTime <- function(episode) {
+    maxlist <- lapply(episode@data, 
+                      function(item){
+                          if(length(item) > 1)
+                              return(max(item$time))
+                      })
+    return(max(unlist(maxlist)))
+}
+
+findMinTime <- function(episode) {
+    maxlist <- lapply(episode@data, 
+                      function(item){
+                          if(length(item) > 1)
+                              return(max(item$time))
+                      })
+    return(max(unlist(maxlist)))
+}
+
+#' Propagate a numerical delta time interval record.
+#' @param record ccRecord
+#' @param delta time frequency in hours
+#' @details when discharge time and admission time are missing, the latest  and
+#' the earliest data time stamp will be used instead.
 #' @export reallocateTimeRecord
 reallocateTimeRecord <- function(record, delta=0.5) {
     newdata <- for_each_episode(record, 
                                 function(e) {
-                                    cat('-')
                                     env <- environment()
+                                    # make sure admin and disc time is correct
+                                    if (class(e@admin_icu_time)[1] != "POSIXct")
+                                        tadmin <- xmlTime2POSIX(e@admin_icu_time)
+                                    else 
+                                        tadmin <- e@admin_icu_time
+
+                                    if (class(e@discharge_icu_time)[1] != "POSIXct")
+                                        tdisc <- xmlTime2POSIX(e@discharge_icu_time)
+                                    else 
+                                        tdisc <- e@discharge_icu_time
+
+                                    if (is.na(tadmin) || is.na(tdisc))
+                                        period_length <- findMaxTime(e)
+                                    else
+                                        period_length <- as.numeric(tdisc - tadmin,
+                                                                units="hours")
+                                    # calling reallocateTime for each data item
                                     lapply(e@data, 
                                            function(d) {
                                                if (length(d) > 1) {
-                                                   maxtime <- env$e@discharge_icu_time - env$e@admin_icu_time
-                                                   maxtime <- as.numeric(maxtime, units="hours")
-                                                   return(reallocateTime(d, maxtime, delta))
+                                                   return(reallocateTime(d, env$period_length, delta))
                                                } else 
                                                    return(d)
                                            })
                                 })
     return(ccRecord() + newdata)
 }
+
+
+
