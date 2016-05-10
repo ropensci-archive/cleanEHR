a4c3d0ae11b514fcb79399ba3d58f22c7197d034
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-10 18:52:09 +0100

    solving problematic time stamps with the format 2015-05-01

--- R/xmlTime2POSIX.r
+++ R/xmlTime2POSIX.r
@@ -2,5 +2,8 @@
 #' e.g. 2014-02-01T03:00:00 -> 2014-02-01 03:00:00
 #' @export xmlTime2POSIX
 xmlTime2POSIX <- function(xml.time){
-    return(as.POSIXct(xml.time, "GMT", format="%Y-%m-%dT%H:%M:%S"))
+    tp <- as.POSIXct(xml.time, "GMT", format="%Y-%m-%dT%H:%M:%S")
+    tp[is.na(tp)] <- as.POSIXct(xml.time[is.na(tp)], "GMT")
+    stopifnot(all(!is.na.POSIXlt(tp)))
+    return(tp)
 }
