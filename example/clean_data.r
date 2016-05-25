56587bb90b0b180284b0e54d8933f463c78b94b3
Sinan Shi <s.shi@ucl.ac.uk>
2016-05-25 09:49:41 +0100

    filter threshold missingness 2d

--- example/clean_data.r
+++ example/clean_data.r
@@ -1,9 +1,13 @@
 # 
 library(ccdata)
 
-if (!exists("ccd_delta_num"))
-    load('../data/delta_num.Rdata')
+if (!exists("ccd"))
+    load('../data/all_patients_pseudo.Rdata')
 
 # 
-#if (!exists("cdt"))
+if (!exists("cdt"))
     cdt <- (new.ccDataTable(ccd_delta_num, "tests/data/test_yml.yml"))
+
+cdt_no_null <- remove_null(cdt)
+
+
