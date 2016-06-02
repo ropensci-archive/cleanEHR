7299dc923450788b99c5e3c0f4cf55acfbe80742
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-02 17:05:38 +0100

    add a faked patient NULL to dataset

--- tests/testthat/test_ccdatatable.r
+++ tests/testthat/test_ccdatatable.r
@@ -1,13 +1,12 @@
 context("Testing ccDataTable")
 
 test_that("test create table",{
-    ccd_delta <- deltaTime(ccd, anonymised=T)
+
+    ccd_delta <- suppressWarnings(deltaTime(ccd_, anonymised=T))
     conf <<- yaml.load_file('../data/test_yml.yml')
     tb <<- ccDataTable2(record=ccd_delta, conf=conf)
     tb$create.table(freq=1)
     # assign table to both origin and clean table
     expect_true(!is.null(tb$origin_table))
     expect_equivalent(tb$origin_table, tb$clean_table)
-
 })
-
