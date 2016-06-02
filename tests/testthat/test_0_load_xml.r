8e58620d1a1601b44d197a4b15d9f47d8141236f
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-02 17:05:38 +0100

    add a faked patient NULL to dataset

--- tests/testthat/test_0_load_xml.r
+++ tests/testthat/test_0_load_xml.r
@@ -1,4 +1,8 @@
 # load test data
 #if (!exists("ccd")) {
-    ccd <<- xml2Data("../data/test_data_10_patients.xml", seq(2), quiet=TRUE)
+    ccd <<- xml2Data("../data/test_data_10_patients.xml", seq(3), quiet=TRUE)
+    # remove the first null patient
+    ccd_ <<- ccRecord()
+    ccd_ <<- ccd_ + ccd[2, 1] + ccd[3, 1]
+
 #}
