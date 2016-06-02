05d33fbe064ee4f0e75571cb6eac894980ea3e5f
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2016-06-02 10:55:40 +0100

    Fixed functions names in untimeit.r

--- example/pipeline/untimeit.r
+++ example/pipeline/untimeit.r
@@ -5,8 +5,8 @@ library(ccdata)
 args = commandArgs(trailingOnly=TRUE)
 
 load(args[1])
-ccd <- reindex(ccd)
+ccd <- reindexRecord(ccd)
 ccd <- deltaTime(ccd, anonymised=T)
-ccd <- uniquePatient(ccd)
+ccd <- uniquePatients(ccd)
 ccd_delta_num <- ccd
 save(ccd_delta_num, file=args[2])
