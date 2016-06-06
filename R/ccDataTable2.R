9fff8ce77ab066da51cf3290b681476cf32b59af
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-06 17:53:19 +0100

    missingness 2d count and filter

--- R/ccDataTable2.R
+++ R/ccDataTable2.R
@@ -28,16 +28,61 @@ create.table = function(freq){
 })
 
 ccDataTable2$methods(
-filter.missingness = function(apply=TRUE){
+filter.missingness = function(recount=FALSE){
     "filter out the where missingness is too low."
-#    a <- copy(.self$origin_table)
-#   print(.self$origin_table)
+    if (recount || is.null(.self$missingness_table))
+        .self$count.missingness()
 
-    return(.self$origin_table[, 1, by=episode_id])
-#   return(.self$origin_table)
-    #mstb[, V1:=NULL]
-
+    thresholds <- 
+        unlist(lapply(.self$conf, 
+                      function(x) x[["missingness_2d"]][["miss_acceptance"]]))
+    
+    # how to make functions data.table awared? so that we can avoid these ugly
+    # indexing.
+    select_index <- rep(TRUE, nrow(.self$missingness_table))
+    for (nt in names(thresholds))
+        select_index <- 
+            select_index & as.vector(.self$missingness_table[, nt, with=FALSE] > thresholds[nt])
+    select_table <- .self$missingness_table[select_index]
+    .self$clean_table <- 
+        merge(select_table, .self$clean_table, by=c("episode_id", "site"))
 })
+
+
+ccDataTable2$methods(
+count.missingness = function() {
+    miss_count <- function(tb) { 
+        cmplt <- function(vec) {
+            length(which(vec!="NA"))/length(vec) * 100 
+        }
+        items <- names(tb)[!names(tb) %in% c("site", "time", "episode_id")]
+        stopifnot(length(items)==1) 
+        flags <- tb[, cmplt(.SD[[items[1]]]), .(episode_id, site)]
+        setnames(flags, c('episode_id', 'site', items))
+        flags
+    }
+    
+    .self$missingness_table <- .self$origin_table[, 1, by=c("episode_id", "site")]
+    .self$missingness_table[, V1:=NULL]
+    setkey(.self$missingness_table, episode_id, site)
+    
+    for (i in names(.self$conf)) {
+        missconf <- .self$conf[[i]][["missingness_2d"]][["labels"]]
+        if(!is.null(missconf)) {
+            for (c in seq(missconf)) {
+                col_name <- names(missconf[c])
+                colr <- missconf[[c]]
+                tbq <- selectTable(.self$record, items_opt=i, freq=colr)
+                setkey(tbq, episode_id, site)
+                oldnm <- names(.self$missingness_table)
+                .self$missingness_table <- 
+                    merge(.self$missingness_table, missingness_count(tbq))
+                setnames(.self$missingness_table, c(oldnm, paste(i, col_name, sep=".")))
+            }
+        }
+    }    
+})
+
 
 
 
@@ -47,6 +92,8 @@ filter.null = function(items=c("episode_id", "site")) {
     for (i in items)
         .self$clean_table <- .self.clean_table[i != "NULL"]
 })
+
+
 
 ccDataTable2$methods(
 reload.conf = function(file) {
--- tests/testthat/test_ccdatatable.r
+++ tests/testthat/test_ccdatatable.r
@@ -1,12 +1,19 @@
-library(data.table)
 context("Testing ccDataTable")
+env <- environment()
+ccd_delta <- suppressWarnings(deltaTime(ccd_, anonymised=T))
+conf <- yaml.load_file('../data/test_yml.yml')
+tb <- ccDataTable2(record=ccd_delta, conf=conf)
 
 test_that("test create table",{
-    ccd_delta <- suppressWarnings(deltaTime(ccd_, anonymised=T))
-    conf <- yaml.load_file('../data/test_yml.yml')
-    tb <- ccDataTable2(record=ccd_delta, conf=conf)
+    tb <- env$tb
     tb$create.table(freq=1)
     # assign table to both origin and clean table
     expect_true(!is.null(tb$origin_table))
     expect_equivalent(tb$origin_table, tb$clean_table)
+})
+
+test_that("test count missingness", {
+    tb <- env$tb
+    tb$count.missingness()
+    tt <<- tb
 })
