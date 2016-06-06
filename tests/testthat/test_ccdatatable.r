c50fe3d9e0d8978c2a0630da28053bc19c240e27
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-06 15:04:34 +0100

    tidyup

--- tests/testthat/test_ccdatatable.r
+++ tests/testthat/test_ccdatatable.r
@@ -6,9 +6,6 @@ test_that("test create table",{
     conf <- yaml.load_file('../data/test_yml.yml')
     tb <- ccDataTable2(record=ccd_delta, conf=conf)
     tb$create.table(freq=1)
-#    print(tb$fun1())
-    print(tb$filter.missingness())
-    print(search())
     # assign table to both origin and clean table
     expect_true(!is.null(tb$origin_table))
     expect_equivalent(tb$origin_table, tb$clean_table)
