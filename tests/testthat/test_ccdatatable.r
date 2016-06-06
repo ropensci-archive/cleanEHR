29a7982a187733b84922eac8d19cabdaa3d3644e
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-06 19:21:11 +0100

    get correct clean table; tests

--- tests/testthat/test_ccdatatable.r
+++ tests/testthat/test_ccdatatable.r
@@ -15,5 +15,39 @@ test_that("test create table",{
 test_that("test count missingness", {
     tb <- env$tb
     tb$count.missingness()
-    tt <<- tb
 })
+
+test_that("test filter missingness", {
+    tb <- env$tb
+    tb$filter.missingness()
+    expect_true(any(class(tb$clean_table)=="data.table"))
+    timestamps <- tb$clean_table[, .N , by=c("site", "episode_id")]
+    print(timestamps)
+    for(i in names(tb$conf)) {
+        nastamps <- tb$clean_table[, length(which(as.character(.SD[[i]]) == "NA")),
+                               by=c("site", "episode_id")]
+        print(nastamps)
+    }
+
+    
+
+
+
+
+
+})
+
+test_that("test the case where no clean_table or missingness_table been
+          initialised.", 
+{
+    tb <- env$tb
+    clean_table <- tb$clean_table
+    # check the case when there is no missingness table
+    tb$missingness_table <- data.table(NULL)
+    tb$clean_table <- data.table(NULL)
+    tb$filter.missingness()
+    expect_true(any(class(tb$clean_table)=="data.table"))
+    expect_equivalent(clean_table, tb$clean_table)
+})
+
+test_that("check the recount argument", {})
