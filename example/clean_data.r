1808a57222bce98b0905664764150690e448efd3
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-16 12:54:16 +0100

    Correct the variable names according to the new yaml file.

--- example/clean_data.r
+++ example/clean_data.r
@@ -9,15 +9,17 @@ dt.sofa <- ccDataTable(conf=yaml.load_file('tests/data/test_yml.yml'), record=cc
 # create table with all selected items in yaml conf with cadance of 1 hour.
 dt.sofa$create.table(freq=1)
 
+dt.sofa$filter.ranges()
+
 # derive the traffic light missingness table.
-dt.sofa$get.missingness()
+#dt.sofa$get.missingness()
 
 # filter data by threshold. (throw out episodes)
-dt.sofa$filter.missingness()
+#dt.sofa$filter.missingness()
 
 # reload - if you want to adjust parameters related with missing rate
-dt.sofa$reload.conf('tests/data/test_yml.yml')
-dt.sofa$filter.missingness()
+#dt.sofa$reload.conf('tests/data/test_yml.yml')
+#dt.sofa$filter.missingness()
 
 # imputation - substituting missing values
-dt.sofa$imputation()
+#dt.sofa$imputation()
