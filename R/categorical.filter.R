a4b7a0b8ce05233dc683fcee070c138518450430
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-22 15:51:38 +0100

    rename data_quality to dfilter.

--- R/categorical.filter.R
+++ R/categorical.filter.R
@@ -4,24 +4,24 @@ ccTable$methods(
     check.categorical = function() {
         "Check individual entries if they are the in the categories specified
         in conf."
-        .self$data_quality$categories <- list()
-        .self$data_quality$categories$entry <- 
+        .self$dfilter$categories <- list()
+        .self$dfilter$categories$entry <- 
             .self$torigin[, c("site", "episode_id", "time"), with=FALSE]
-        .self$data_quality$categories$episode <- 
+        .self$dfilter$categories$episode <- 
             .self$torigin[, .N, by=c("site", "episode_id")]
-        setkey(.self$data_quality$categories$entry, "site", "episode_id")
-        setkey(.self$data_quality$categories$episode, "site", "episode_id")
+        setkey(.self$dfilter$categories$entry, "site", "episode_id")
+        setkey(.self$dfilter$categories$episode, "site", "episode_id")
 
         for (i in .self$items) {
             ctg <- .self$conf[[i]][['categories']]
             if (!is.null(ctg)) { 
                 # fill the entry table
-                .self$data_quality$categories$entry[[i]] <-
+                .self$dfilter$categories$entry[[i]] <-
                     .self$torigin[[i]] %in% c(names(ctg), "NA", NA)
                 # fill the episode table
                 epcheck <- 
-                    .self$data_quality$categories$entry[, c("site", "episode_id", i), with=FALSE]
-                .self$data_quality$categories$episode[[i]] <-
+                    .self$dfilter$categories$entry[, c("site", "episode_id", i), with=FALSE]
+                .self$dfilter$categories$episode[[i]] <-
                     epcheck[, all(.SD[[i]]), by=c("site", "episode_id")]$V1
             }
         }
@@ -29,11 +29,12 @@ ccTable$methods(
 
 
 ccTable$methods(
-    filter.categorical = function() {
+    filter.categorical = function(apply=TRUE) {
         "Substitute original wrong categorical values with "
         for (iname in names(.self$conf)) {
             this.conf <- .self$conf[[iname]]
             spec <- as.vector(unlist(this.conf)['apply.categorical'])
-            .self$spec2function(spec)(iname, .self$data_quality$categories)
+            .self$spec2function(spec)(iname, .self$dfilter$categories)
+            
         }
     })
--- R/range.filter.R
+++ R/range.filter.R
@@ -19,17 +19,17 @@ inrange <- function(v, range) {
 ccTable$methods(
     filter.ranges = function(select='red') {
         rgnum <- list('red'=1, 'amber'=2, 'green'=3)
-        if(is.null(.self$data_quality$range) || nrow(.self$data_quality$range) != nrow(.self$tclean))
+        if(is.null(.self$dfilter$range) || nrow(.self$dfilter$range) != nrow(.self$tclean))
             .self$get.ranges()
-        for(item in names(.self$data_quality$range)) 
-            .self$tclean[[item]][.self$data_quality$range[[item]] < rgnum[[select]]] <- NA 
+        for(item in names(.self$dfilter$range)) 
+            .self$tclean[[item]][.self$dfilter$range[[item]] < rgnum[[select]]] <- NA 
     }
 )
 
 ccTable$methods(
     get.ranges = function(){
-        if (is.null(.self$data_quality$range))
-            .self$data_quality$range <- data.table(seq(nrow(.self$torigin)))#.self$torigin[,c('site', 'episode_id'), with=F]
+        if (is.null(.self$dfilter$range))
+            .self$dfilter$range <- data.table(seq(nrow(.self$torigin)))#.self$torigin[,c('site', 'episode_id'), with=F]
         rgnum <- list('red'=1, 'amber'=2, 'green'=3)
         for(item_name in names(.self$conf)) {
             item <- .self$conf[[item_name]]
@@ -43,7 +43,7 @@ ccTable$methods(
                     rgclass[which(inrange(.self$torigin[[item_name]], irg))] <- 
                         rgnum[[rg_label]]
                 }
-                .self$data_quality$range[[item_name]] <- rgclass
+                .self$dfilter$range[[item_name]] <- rgclass
             }
         }
     }
