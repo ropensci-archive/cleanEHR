e2b8a781649d412d2bf6db62a4d6f0607a9da154
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-22 14:02:12 +0100

    create episode level update of categorical filter.

--- R/categorical.filter.R
+++ R/categorical.filter.R
@@ -4,15 +4,26 @@ ccTable$methods(
     check.categorical = function() {
         "Check individual entries if they are the in the categories specified
         in conf."
-        .self$data_quality[['categories']] <- list()
-        .self$data_quality[['categories']][['entry']] <- 
+        .self$data_quality$categories <- list()
+        .self$data_quality$categories$entry <- 
             .self$torigin[, c("site", "episode_id", "time"), with=FALSE]
+        .self$data_quality$categories$episode <- 
+            .self$torigin[, .N, by=c("site", "episode_id")]
+        setkey(.self$data_quality$categories$entry, "site", "episode_id")
+        setkey(.self$data_quality$categories$episode, "site", "episode_id")
 
         for (i in .self$items) {
             ctg <- .self$conf[[i]][['categories']]
-            if (!is.null(ctg))
-                .self$data_quality[['categories']][['entry']][[i]] <-
+            if (!is.null(ctg)) { 
+                # fill the entry table
+                .self$data_quality$categories$entry[[i]] <-
                     .self$torigin[[i]] %in% c(names(ctg), "NA", NA)
+                # fill the episode table
+                epcheck <- 
+                    .self$data_quality$categories$entry[, c("site", "episode_id", i), with=FALSE]
+                .self$data_quality$categories$episode[[i]] <-
+                    epcheck[, all(.SD[[i]]), by=c("site", "episode_id")]$V1
+            }
         }
     })
 
@@ -26,4 +37,3 @@ ccTable$methods(
             .self$spec2function(spec)(iname, .self$data_quality$categories)
         }
     })
-
