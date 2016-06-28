8c2a873512f0c9d86ab6cf228ae824afff30fc60
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-28 11:06:31 +0100

    modify examples

--- example/clean_data.r
+++ example/clean_data.r
@@ -8,7 +8,7 @@ ccd <- ccd_delta_num
 dt.sofa <- ccTable(conf=yaml.load_file('tests/data/test_sofa.yml'), record=ccd)
 # create table with all selected items in yaml conf with cadance of 1 hour.
 dt.sofa$create.table(freq=1)
-
+#dt.sofa$filter.nodata()
 dt.sofa$filter.ranges()
 dt.sofa$filter.category()
 dt.sofa$filter.missingness()
