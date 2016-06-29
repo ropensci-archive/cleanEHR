83d77dcbab924b71b32ed6284be74b4ce061e6aa
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-29 11:40:45 +0100

    update configuration

--- example/clean_data.r
+++ example/clean_data.r
@@ -8,11 +8,11 @@ ccd <- ccd_delta_num
 dt.sofa <- ccTable(conf=yaml.load_file('tests/data/test_sofa.yml'), record=ccd)
 # create table with all selected items in yaml conf with cadance of 1 hour.
 dt.sofa$create.table(freq=1)
-#dt.sofa$filter.nodata()
 dt.sofa$filter.ranges()
 dt.sofa$filter.category()
 dt.sofa$filter.missingness()
+dt.sofa$filter.nodata()
 dt.sofa$apply.filters()
 
 # imputation - substituting missing values
-#dt.sofa$imputation()
+dt.sofa$imputation()
