962832d0f50244a14067d51fd01f3c03bf42f00a
Sinan Shi <s.shi@ucl.ac.uk>
2016-06-29 12:46:33 +0100

    fix #63 range filter bug

--- example/clean_data.r
+++ example/clean_data.r
@@ -5,14 +5,14 @@ if (!exists("ccd_delta_num"))
     load('../data/delta_num.Rdata')
 ccd <- ccd_delta_num
 
-dt.sofa <- ccTable(conf=yaml.load_file('tests/data/test_sofa.yml'), record=ccd)
-# create table with all selected items in yaml conf with cadance of 1 hour.
+dt.sofa <- ccTable(conf=yaml.load_file('tests/data/ANALYSIS_REF.yaml'), record=ccd)
 dt.sofa$create.table(freq=1)
 #dt.sofa$filter.nodata()
 dt.sofa$filter.ranges()
-dt.sofa$filter.category()
-dt.sofa$filter.missingness()
-dt.sofa$apply.filters()
+#dt.sofa$filter.category()
+#dt.sofa$filter.missingness()
+#dt.sofa$filter.nodata()
+#dt.sofa$apply.filters()
 
 # imputation - substituting missing values
 #dt.sofa$imputation()
