6b858e12407879d60b5a86a08c74720be40a60a4
Sinan Shi <s.shi@ucl.ac.uk>
2016-07-18 12:07:08 +0100

    unique spell

--- R/unique.spell.R
+++ R/unique.spell.R
@@ -2,21 +2,38 @@
 #' @param record ccRecord
 #' @return data.table contains spell id.
 #' @export unique.spell
-unique.spell <- function(record) {
-    env <- environment()
+unique.spell <- function(rec) {
     tb <- list()
-    for_each_episode(record, 
+    env <- environment()
+    for_each_episode(rec, 
                      function(x) {
-                         env$tb[[length(tb)+1]] <-
-                             .simple.data.frame(list(site=x@site_id,
-                                                     episode_id=x@episode_id,
-                                                     nhs_num=x@nhs_number,
-                                                     pas_num=x@pas_number,
+                         env$tb[[length(tb) + 1]] <- 
+                             .simple.data.frame(list(
+                                                     site=x@site_id, 
+                                                     episode_id=x@episode_id, 
+                                                     nhs_number=x@nhs_number,
+                                                     pas_number=x@pas_number, 
                                                      admt=x@admin_icu_time,
-                                                     dict=x@discharge_icu_time))
+                                                     dict=x@discharge_icu_time, 
+                                                     parse_time=x@parse_time,
+                                                     file_origin=x@file_origin))
                      })
-    tb <- rbindlist(tb)
+    tb <- rbindlist(tb, fill=TRUE)
+    setnames(tb, c("site", "episode_id", "nhs_number", "pas_number", "admt",
+                   "dict", "parse_time", "file_origin"))
+    tb$dict <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$dict)),
+                                          origin="1970-01-01"))
+    tb$admt <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$admt)),
+                                          origin="1970-01-01"))
+    tb[["pid"]] <- tb$nhs_number
+    tb[["pid"]][(tb$pid=="NULL")] <- tb[["pas_number"]][tb$pid=="NULL"]
+
     tb <- tb[!(site=="NULL" & episode_id=="NULL")]
-    tb[, patient_id:=.GRP, by=c("site", "episode_id")]
+    tb[, spell:=.GRP, by="pid"]
+    setkey(tb, "admt")
     tb
+
 }
+
+
+
