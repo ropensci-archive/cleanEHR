d441bdbd734d923af8371877f521ed57418ee52d
Sinan Shi <s.shi@ucl.ac.uk>
2016-08-08 13:48:03 +0100

    adding sql.newdb() & sql.add.demographic()

--- R/sql_demographic.r
+++ R/sql_demographic.r
@@ -18,7 +18,6 @@ sql.demographic.table <- function(record, dtype=TRUE) {
             env$demogls[[length(env$demogls) + 1]] <- .simple.data.frame(demog.data)
         })
     demogt <- rbindlist(demogls)
-    nm <<-names(demogt)
     setnames(demogt, code2stname(names(demogt)))
 
     if (dtype) {
@@ -40,13 +39,37 @@ sql.init.postgres <- function() {
             sqldf.RPostgreSQL.port =5432)
 }
 
+#' Load SQL command from a file in which the SQL commands are recorded.
+#' @param path the full path of the SQL file.
+#' @return a string of command.
+#' @export sql.load_file
+sql.load_file <- function(path) {
+    sqlfile <- file(path)
+    cmd <- paste(readLines(path), collapse="")
+    close(sqlfile)
+    return(cmd)
+}
+
+#' Remove all the existing tables from the database and load the new schema
+#' file.
+#' @param schema is the full path of the selected schema file.
+#' @export sql.newdb
+sql.newdb <- function(schema=NULL) {
+    if (is.null(schema))
+        schema <- paste(find.package("ccdata"), "sql/create_table.sql", sep="/") 
+
+    sql.init.postgres()
+    cmd <- sql.load_file(schema)
+    sqldf(cmd)
+}
+
 #' Add demographic table to the postgreSQL table.
 #' @param record
 #' @export sql.add.demographic
 sql.add.demographic <- function(record) {
     sql.init.postgres()
-    
     sqldf('DROP TABLE IF EXISTS demographic;')
+    drv <- dbDriver("PostgreSQL")
     con <- dbConnect(drv, host="localhost", user="sinan", dbname="postgres")
     demogt <- sql.demographic.table(record, dtype=TRUE)
     dbWriteTable(con, "demographic", demogt)
