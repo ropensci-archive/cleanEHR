2033006b13796d27100684b43fc1a4a71188db72
Sinan Shi <s.shi@ucl.ac.uk>
2016-08-11 12:02:57 +0100

    rearrange R files

--- R/miss_2d.r
+++
@@ -1,9 +0,0 @@
-#' getMissingRate
-#' @export get_missing_rate 
-get_missing_rate <- function(record, item, freq, sites=NULL, years=NULL) {
-    datalist <- select_data(record, item=item, sites=sites, years=years, propgt=TRUE, 
-                            freq=freq)
-    missing_rate <- sapply(datalist, function(x)
-                  length(which(x$val=="NA"))/length(x$val))
-    missing_rate
-}
