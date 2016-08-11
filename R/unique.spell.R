0608bc709dc661a639648be36154d9037c96a153
Sinan Shi <s.shi@ucl.ac.uk>
2016-08-11 11:49:18 +0100

    new unique spell

--- R/unique.spell.R
+++ R/unique.spell.R
@@ -1,38 +1,54 @@
+
+
+
 #' find the unique spell ID. 
 #' @param record ccRecord
 #' @return data.table contains spell id.
 #' @export unique.spell
-unique.spell <- function(rec) {
+unique.spell <- function(rec, duration=2) {
     tb <- list()
     env <- environment()
     for_each_episode(rec, 
-                     function(x) {
-                         env$tb[[length(tb) + 1]] <- 
-                             .simple.data.frame(list(
-                                                     site=x@site_id, 
-                                                     episode_id=x@episode_id, 
-                                                     nhs_number=x@nhs_number,
-                                                     pas_number=x@pas_number, 
-                                                     admt=x@admin_icu_time,
-                                                     dict=x@discharge_icu_time, 
-                                                     parse_time=x@parse_time,
-                                                     file_origin=x@file_origin))
-                     })
+        function(x) {
+            env$tb[[length(tb) + 1]] <- 
+                .simple.data.frame(list(
+                    site      = as.character(x@site_id), 
+                    episode_id= as.character(x@episode_id), 
+                    nhs_number= as.character(x@nhs_number),
+                    pas_number= as.character(x@pas_number), 
+                    admt      = as.numeric(x@admin_icu_time),
+                    dict      = as.numeric(x@discharge_icu_time), 
+                    file_origin=as.character(x@file_origin)))
+        }
+    )
     tb <- rbindlist(tb, fill=TRUE)
-    setnames(tb, c("site", "episode_id", "nhs_number", "pas_number", "admt",
-                   "dict", "parse_time", "file_origin"))
-    tb$dict <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$dict)),
-                                          origin="1970-01-01"))
-    tb$admt <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$admt)),
-                                          origin="1970-01-01"))
-    tb[["pid"]] <- tb$nhs_number
-    tb[["pid"]][(tb$pid=="NULL")] <- tb[["pas_number"]][tb$pid=="NULL"]
+     setnames(tb, c("site", "episode_id", "nhs_number", "pas_number", "admt",
+                    "dict", "file_origin"))
+     tb$dict <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$dict)),
+                                           origin="1970-01-01"))
+     tb$admt <- suppressWarnings(as.POSIXct(as.numeric(unclass(tb$admt)),
+                                           origin="1970-01-01"))
+     tb[["id"]] <- tb$nhs_number
+     tb[["id"]][(tb$id=="NULL")] <- tb[["pas_number"]][tb$id=="NULL"]
+    
+     tb <- tb[!(site=="NULL" | episode_id=="NULL" | id=="NULL" | is.na(dict) | is.na(admt))]
 
-    tb <- tb[!(site=="NULL" & episode_id=="NULL")]
-    tb[, spell:=.GRP, by="pid"]
-    setkey(tb, "admt")
-    tb
+     tb[, pid:=.GRP, by="id"]
 
+     short.time.group <- function(sd) {
+         zeroday <- 0
+         if (length(sd[[1]]) == 1)
+             return(zeroday)
+         adm <- sd$admt[1:length(sd$admt)-1]
+         dic <- sd$dict[2:length(sd$dict)]
+         return(c(zeroday, difftime(dic, adm, units="days")))
+     }
+     setkey(tb, "pid", "admt", "dict")
+     tb[, diffday:=short.time.group(.SD), by="pid"]
+
+     spell <- Reduce(sum, tb$diffday == 0 | tb$diffday > duration, accumulate=T)
+     tb$spell <- spell
+     return(tb)
 }
 
 
