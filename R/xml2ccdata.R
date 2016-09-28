cc1b6e9b57e6240271c1f71e25d7561c1e6d0104
Sinan Shi <s.shi@ucl.ac.uk>
2016-09-28 18:46:04 +0100

    rearrange the layout to make the code more compact

--- R/xml2ccdata.R
+++ R/xml2ccdata.R
@@ -154,10 +154,131 @@ xmlEpisodeToList <- function(episode_node) {
                else{
                    print(x)
                    stop("0 or more than 4 columns here!")
-               
                }
 })
-
-
     return(node_env$ccdata)
+}
+
+
+#' load xml clinical data
+#' @return the root of the xml data
+#' @export xmlLoad
+xmlLoad <- function(file) {
+    file.parse <- xmlParse(file)
+    xml.root <- xmlRoot(file.parse)
+    return(xml.root)
+}
+
+#' get the episode data from xml 
+#' @param xml.root root of xml data returned by xmlLoad()
+#' @export getXmlepisode
+getXmlepisode <- function(xml.root, id) {
+    xml.root[[1]][[2]][[id]]
+}
+
+#' Extract the original file name from a path and file removing
+#' all the suffixes.
+#' @param pathfile a particular file name which may have a suffix
+#' @param removestr last bit from the original filename
+#' @return string
+#' @export extract_file_origin
+extract_file_origin <- function(pathfile, removestr='.xml'){
+  split_path <- unlist(strsplit(pathfile, "/"))
+  filename <- split_path[length(split_path)]
+  original <- unlist(strsplit(filename, removestr))
+  return(paste(original[1], removestr, sep=""))
+  }
+
+
+#' convert xml data to ccdata format - [ready for retire]
+#' @param file xml file name
+#' @return ccdata 
+#' @export xml2Data
+xml2Data <- function (file, select.episode=NULL, quiet=TRUE, xml=NULL,
+                      file_origin="NA", parse_time=Sys.time()){
+  if (is.null(xml)) {
+    if (file_origin == "NA") {
+      file_origin <- extract_file_origin(file)
+    }
+    xml <- xmlLoad(file)
+  }
+
+    episode.num <- xmlSize(xml[[1]][[2]])
+    if(is.null(select.episode))
+        select.episode <- seq(episode.num)
+
+
+    if (!quiet)
+        pb <- txtProgressBar(min = min(select.episode)-1, 
+                             max = max(select.episode), style = 3)
+    record <- ccRecord()
+
+    for(episode.id in select.episode){
+        episode <- getXmlepisode(xml, episode.id)
+        episode_list <- tryCatch(xmlEpisodeToList(episode), 
+                                 error=function(err) {
+                                     cat(paste(err, "episode.id = ", episode.id, "\n"))
+                                     stop()
+                                 })
+        episode_i <- ccEpisode(episode_list)
+        episode_i@file_origin <- file_origin
+        episode_i@parse_time <- parse_time
+        record <- record + episode_i
+
+        if (!quiet)
+            setTxtProgressBar(pb, episode.id)
+    }
+    if (!quiet)
+        cat("\n")
+
+    setkey(record@nhs_numbers, nhs_number)
+    setkey(record@pas_numbers, pas_number)
+
+    return(record)
+}
+
+
+
+#' convert xml data to ccdata format - [ready for retire]
+#' @param file xml file name
+#' @return ccdata 
+#' @export xml2Data2
+xml2Data2 <- function (file, select.episode=NULL, quiet=TRUE, xml=NULL,
+                      file_origin="NA", parse_time=Sys.time()){
+  if (is.null(xml)) {
+    if (file_origin == "NA") {
+      file_origin <- extract_file_origin(file)
+    }
+    xml <- xmlLoad(file)
+  }
+
+    episode.num <- xmlSize(xml[[1]][[2]])
+    if(is.null(select.episode))
+        select.episode <- seq(episode.num)
+
+
+    if (!quiet)
+        pb <- txtProgressBar(min = min(select.episode)-1, 
+                             max = max(select.episode), style = 3)
+    eps <- list()
+
+    for(episode.id in select.episode){
+        episode <- getXmlepisode(xml, episode.id)
+        episode_list <- tryCatch(xmlEpisodeToList(episode), 
+                                 error=function(err) {
+                                     cat(paste(err, "episode.id = ", episode.id, "\n"))
+                                     stop()
+                                 })
+        eps[[episode.id]] <- new.episode(episode_list, file_origin, parse_time)
+
+        if (!quiet)
+            setTxtProgressBar(pb, episode.id)
+    }
+
+    record <- ccRecord2() + eps
+    if (!quiet)
+        cat("\n")
+
+
+    return(record)
 }
