658e76e1cce573ac255461d69497aea5ec936c12
Sinan Shi <s.shi@ucl.ac.uk>
2016-09-29 10:27:10 +0100

    xml2Data2 -> xml2Data

--- R/xml2ccdata.R
+++ R/xml2ccdata.R
@@ -211,55 +211,6 @@ xml2Data <- function (file, select.episode=NULL, quiet=TRUE, xml=NULL,
     if (!quiet)
         pb <- txtProgressBar(min = min(select.episode)-1, 
                              max = max(select.episode), style = 3)
-    record <- ccRecord()
-
-    for(episode.id in select.episode){
-        episode <- getXmlepisode(xml, episode.id)
-        episode_list <- tryCatch(xmlEpisodeToList(episode), 
-                                 error=function(err) {
-                                     cat(paste(err, "episode.id = ", episode.id, "\n"))
-                                     stop()
-                                 })
-        episode_i <- ccEpisode(episode_list)
-        episode_i@file_origin <- file_origin
-        episode_i@parse_time <- parse_time
-        record <- record + episode_i
-
-        if (!quiet)
-            setTxtProgressBar(pb, episode.id)
-    }
-    if (!quiet)
-        cat("\n")
-
-    setkey(record@nhs_numbers, nhs_number)
-    setkey(record@pas_numbers, pas_number)
-
-    return(record)
-}
-
-
-
-#' convert xml data to ccdata format - [ready for retire]
-#' @param file xml file name
-#' @return ccdata 
-#' @export xml2Data2
-xml2Data2 <- function (file, select.episode=NULL, quiet=TRUE, xml=NULL,
-                      file_origin="NA", parse_time=Sys.time()){
-  if (is.null(xml)) {
-    if (file_origin == "NA") {
-      file_origin <- extract_file_origin(file)
-    }
-    xml <- xmlLoad(file)
-  }
-
-    episode.num <- xmlSize(xml[[1]][[2]])
-    if(is.null(select.episode))
-        select.episode <- seq(episode.num)
-
-
-    if (!quiet)
-        pb <- txtProgressBar(min = min(select.episode)-1, 
-                             max = max(select.episode), style = 3)
     eps <- list()
 
     for(episode.id in select.episode){
