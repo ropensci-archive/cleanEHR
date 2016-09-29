650f245dc811517d160a6901007ac9efaabd54f4
Sinan Shi <s.shi@ucl.ac.uk>
2016-09-29 15:08:39 +0100

    a better makefile

--- inst/pipeline/create_table.r
+++
@@ -1,7 +0,0 @@
-library(ccdata)
-# load
-
-select_data <- c("Heart rate", "PaO2/FiO2 ratio", "Sex", "Admission type", "Date
-                of discharge from your hospital")
-                
-table <- ccRecord2Table(ccd, "NIHR_HIC_ICU_0108")
