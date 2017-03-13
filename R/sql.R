6dac69d1dec8a6536ca95808e489fcc1313024a3
Sinan Shi <s.shi@ucl.ac.uk>
2017-03-13 17:09:29 +0000

    exporting longitudinal tables for SQL injection

---
+++ R/sql.R
@@ -0,0 +1,54 @@
+#' Extract a variable from ccRecord object to a table.
+#' 
+#' The output table will contain site_id, episode_id, time, val, and possible
+#' meta data column if the variable is longitudinal. Otherwise only site_id,
+#' episode_id and the value will be presented. 
+#' @param ccd ccRecord object 
+#' @param varname the short name of the selected variable, see ITEM_REF
+#' @export 
+rec2tb <- function(ccd, varname) {
+
+    varcode <- stname2code(varname)
+    if (is.na(names(stname2code(varname)))) 
+        stop("The short name ", varname, " cannot be found.")
+
+    vartb.lst <- 
+        for_each_episode(ccd, 
+        function(x) {
+            if (is.null(x@data[[varcode]]))
+                return(NULL)
+            else 
+                return(cbind(.simple.data.frame(
+                      list(episode_id=x@episode_id, 
+                           site_id=x@site_id)),
+                      x@data[[varcode]]))
+        })
+    vartb <- rbindlist(vartb.lst, fill=TRUE)
+
+    # Rename the columns 
+    # The sequence of time and item2d column is not certain, that's why we have
+    # to search the index of item2d first. 
+    if (ncol(vartb) == 3) # non-longitudinal variables
+        names(vartb) <- c("episoe_id", "site_id", varname)
+    if (ncol(vartb) > 3) # longitudinal 
+        names(vartb) <- gsub(x=names(vartb), "item2d", "val")
+
+    return(vartb)
+}
+
+
+#' Export all the longitudinal fields as tables 
+#'
+#' All the result tables will be wrapped by a list, indexing with short names of
+#' the fields. 
+#' @param ccd ccRecord object
+#' @export
+export.lontb <- function(ccd) {
+    l.stnames <- unlist(lapply(cleanEHR:::ITEM_REF, 
+                  function(x) {
+                      if(!is.null(x$NHICdtCode))
+                          return(x$shortName)
+                  }))
+    names(l.stnames) <- l.stnames # the result can be indexed via short names
+    return(lapply(l.stnames, function(x) rec2tb(ccd, x)))
+}
---
+++ tests/testthat/test_sql.r
@@ -0,0 +1,10 @@
+context("Testing SQL functionalities")
+
+test_that("Export SQL variable tables from ccRecord", {
+   expect_error(rec2tb('nonsense'))
+   expect_true("data.frame" %in% class(rec2tb(ccd, "h_rate")))
+
+   ltb <- export.lontb(ccd)
+   expect_equal(class(ltb), "list")
+   expect_true(all(stname2code(names(ltb)) != "NA"))
+})
