4fc539c326926bf78fc949e153008a0de33a75ec
Sinan Shi <s.shi@ucl.ac.uk>
2017-04-17 21:18:30 +0200

    remove outdated files

--- inst/pipeline/extract_data.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/Rscript
-
-library(cleanEHR)
-
-args = commandArgs(trailingOnly=TRUE)
-
-cat("extract patients from", c(args[1], "\n"))
-ccd <- xml2Data(args[1])
-save(ccd, file=paste(args[1], ".RData", sep="_"))
