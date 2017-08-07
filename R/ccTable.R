fedfd0c885c3b7a003f460ee329defe41c1b7739
Sinan Shi <s.shi@ucl.ac.uk>
2017-08-07 10:53:04 +0200

    remove pipeline

--- R/ccTable.R
+++ R/ccTable.R
@@ -220,3 +220,204 @@ ccTable$methods(
         else 
             warning("The original table is NULL, hence no cleaning process has been performed.")
     })
+
+
+#' Create wide table from ccRecord
+#' 
+#' @param record ccRecord
+#' @param items_opt character vectors. Items (HIC code) selected in item_opt are optional items, which will be automatically 
+#' filled when item is missing. 
+#' @param items_obg obligatory items that is obligatory; Any episode that doesn't contain
+#' item in this vector will be removed.
+#' @param freq numeric cadence in hour. 
+#' @param return_list logical if TRUE return as a list.  
+#' @return data.table
+#' @export selectTable
+selectTable <- function(record, items_opt=NULL, items_obg=NULL, freq,
+                        return_list=FALSE) {
+    all_items <- c(items_opt, items_obg)
+    if (is.null(all_items))
+        stop('both items_opt and items_obg are NULL')
+
+    env <- environment()
+    lt <- list()
+    stopifnot(is.list(env$lt)) # totally redundent, just to avoid an anonying
+    # note says env is assigned but not used!
+    for_each_episode(record,
+                     function(ep) {
+                         if (all(items_obg %in% names(ep@data))) {
+                             result <- list()
+                             period_length <- getEpisodePeriod(ep)
+                             # getEpisodePeriod will return NULL when no 2D
+                             # data been found. 
+                             if (!is.null(period_length)) {
+                                 if (period_length > 0 ) {
+                                     result <- append(result,
+                                                      itemsToDataFrame(ep, all_items,
+                                                                       period_length,
+                                                                       freq))
+                                     nlength <- length(result[["time"]])
+                                     result[["site"]] <- rep(ep@site_id, nlength)
+                                     result[["episode_id"]] <- rep(ep@episode_id, nlength)
+                                     env$lt[[length(lt) + 1]]<- .simple.data.frame(result)
+                                 }
+                             }
+                         }
+                     })
+    if (return_list)
+        return(lt)
+
+    # fill is true because meta data column can be missing. 
+    dt <- rbindlist(lt, fill=TRUE) 
+
+   
+    # Adding missing meta columns to keep the 2d wide consistent. 
+    code.has.meta <- names(unlist(sapply(ITEM_REF, function(x) x$NHICmetaCode)))
+    for (i in all_items) {
+        meta.code <- paste(i, "meta", sep=".")
+        if (i %in% code.has.meta & !(meta.code %in% names(dt))) {
+            dt[[meta.code]] <- rep("NA", nrow(dt))
+        }
+    }
+
+    # convert data type 
+    for (i in all_items)
+        dt[[i]] <- suppressWarnings(.which.datatype(i)(as.character(dt[[i]])))
+
+
+    return(dt)
+}
+
+itemsToDataFrame <- function(ep, items, period_length, freq) {
+    listmatrix <- list()
+    time <- seq(0, period_length, freq)
+
+    listmatrix[["time"]] <- time
+
+    for (i in items) {
+        if (length(ep@data[[i]]) == 1) {
+            listmatrix[[i]] <- rep(ep@data[[i]], length(time))
+        }
+        else {
+            if ("time" %in% names(ep@data[[i]])) {
+                new <- reallocateTime(ep@data[[i]], period_length, freq)
+                listmatrix[[i]] <- new$val
+                if ("meta" %in% names(ep@data[[i]]))
+                    listmatrix[[paste(i, "meta", sep=".")]] <- new$meta
+            }
+            else
+                listmatrix[[i]] <- rep("NA", length(time))
+        }
+    }
+    return(listmatrix)
+}
+
+
+#' @importFrom Rcpp evalCpp
+#' @useDynLib cleanEHR 
+reallocateTime <- function(d, t_discharge, frequency) {
+    d_ <- d
+    stopifnot(any(names(d) == "time"))
+    stopifnot(any(names(d) == "item2d"))
+    stopifnot(class(d$time) == "numeric")
+    return(reallocateTime_(d_, t_discharge, frequency))
+}
+
+
+findMaxTime <- function(episode) {
+    get2dTime <- function(episode){
+        time_lt <- 
+            lapply(episode@data, 
+                   function(item){
+                       if(length(item) > 1) {
+                           if (!is.numeric(item$time))
+                               item$time <-
+                                   as.numeric(as.character(item$time))
+                               return(max(item$time))
+                       }
+                   })
+        tm <- unlist(time_lt)
+        tm
+    }
+    tm <- get2dTime(episode)
+    if (is.null(tm))
+        return(NULL)
+    else
+        return(max(tm))
+}
+
+
+#' Get the length of stay based on the first and the last data point. 
+#' 
+#' @param e ccEpisode object.
+#' @param unit character string.  Units in which the results are desired. Can be abbreviated.
+#' @return length of stay
+#' @export getEpisodePeriod
+getEpisodePeriod <- function (e, unit="hours") {
+    # pseudo delta period, see addPseudoTime()
+    if (class(e@t_discharge)[1] == "numeric")
+        return(e@t_discharge)
+
+    if (class(e@t_admission)[1] != "POSIXct")
+        tadm <- xmlTime2POSIX(as.character(e@t_admission), allow=T)
+    else 
+        tadm <- e@t_admission
+    if (class(e@t_discharge)[1] != "POSIXct")
+        tdisc <- xmlTime2POSIX(as.character(e@t_discharge), allow=T)
+    else 
+        tdisc <- e@t_discharge
+
+    # The failure of POSIX conversion indicates that this episode is either 
+    # anonymised or has a missing or incorrect value of discharge or admission
+    # time. 
+    if (is.na(tadm) || is.na(tdisc))
+        period_length <- findMaxTime(e)
+    else {
+        if (any(is.null(tdisc), is.null(tadm)))
+            period_length <- NULL
+        else
+            period_length <- as.numeric(tdisc - tadm,
+                                        units=unit)
+    }
+    # in cases that tdisc == tadm
+    if (!is.null(period_length)) {
+        if (period_length == 0)
+            period_length <- period_length + 1
+    }
+
+    if (is.null(period_length))
+        warning("This episode does not have any time series data: ", 
+                " episode_id = ", e@episode_id, 
+                " nhs_number = ", e@nhs_number, 
+                " pas_number = ", e@pas_number,
+                " period_length = ", period_length, "\n")
+
+
+    return(period_length)
+}
+
+#' Propagate a numerical delta time interval record.
+#' @param record ccRecord
+#' @param delta time frequency in hours
+#' @details when discharge time and admission time are missing, the latest  and
+#' the earliest data time stamp will be used instead.
+#' @export reallocateTimeRecord
+reallocateTimeRecord <- function(record, delta=0.5) {
+    reallocate.episode <- function(e) {
+        env <- environment()
+        # make sure admin and disc time is correct
+        period_length <- getEpisodePeriod(e)
+        if (period_length < 0)  warning("period length < 0")
+        
+        # calling reallocateTime for each data item
+        new.episode(lapply(e@data, 
+               function(d) {
+                   if (length(d) > 1) {
+                       return(reallocateTime(d, env$period_length, delta))
+                   } else 
+                       return(d)
+               }))
+    }
+    newdata <- for_each_episode(record, reallocate.episode)
+    return(ccRecord() + newdata)
+}
