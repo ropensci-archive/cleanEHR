9caaa18daae56910472e512c505d26220e252627
Sinan Shi <s.shi@ucl.ac.uk>
2017-08-07 10:41:44 +0200

    sql.demographic.table -> demographic.table

--- tests/testthat/test_report.r
+++ tests/testthat/test_report.r
@@ -7,7 +7,7 @@ test_that("file level summary",{
 
 
 test_that("table1", {
-    demg <- suppressWarnings(sql.demographic.table(ccd))
+    demg <- suppressWarnings(demographic.table(ccd))
     table1(demg, "SEX", return.data=T)
     expect_error(table1(demg, "non_nhic", return.data=T))
     expect_error(table1(demg, "h_rate", return.data=T)) # need to be categorical data
@@ -15,7 +15,7 @@ test_that("table1", {
 
 
 test_that("demographic data completeness", {
-    demg <- suppressWarnings(sql.demographic.table(ccd))
+    demg <- suppressWarnings(demographic.table(ccd))
     tb <- demographic.data.completeness(demg, return.data=T)
     ndemg <- length(which(sapply(cleanEHR:::ITEM_REF, function(x) 
                                  x$Classification1=="Demographic")))
