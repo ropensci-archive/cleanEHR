ebbdbec4e3ea5c6c239888b24c5ac49a5518a5d5
Sinan Shi <s.shi@ucl.ac.uk>
2017-08-23 11:05:38 +0200

    import evalRcpp only

--- R/ccRecord.R
+++ R/ccRecord.R
@@ -1,7 +1,7 @@
 #' @import XML
 #' @import data.table
 #' @import yaml
-#' @import Rcpp
+#' @importFrom Rcpp evalCpp
 NULL
 
 
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,7 +7,7 @@ using namespace Rcpp;
 
 // reallocateTime_
 DataFrame reallocateTime_(DataFrame d, const float t_discharge, const float frequency);
-RcppExport SEXP cleanEHR_reallocateTime_(SEXP dSEXP, SEXP t_dischargeSEXP, SEXP frequencySEXP) {
+RcppExport SEXP _cleanEHR_reallocateTime_(SEXP dSEXP, SEXP t_dischargeSEXP, SEXP frequencySEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -17,4 +17,14 @@ BEGIN_RCPP
     rcpp_result_gen = Rcpp::wrap(reallocateTime_(d, t_discharge, frequency));
     return rcpp_result_gen;
 END_RCPP
+}
+
+static const R_CallMethodDef CallEntries[] = {
+    {"_cleanEHR_reallocateTime_", (DL_FUNC) &_cleanEHR_reallocateTime_, 3},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_cleanEHR(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
